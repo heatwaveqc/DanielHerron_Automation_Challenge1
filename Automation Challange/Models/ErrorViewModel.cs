a0d153cd27e9b4aefbd52c8333dba7d6249e5499
Daniel Herron <heatwaveqc@yahoo.com>
2019-01-14 12:50:52 -0600

    Solution

---
+++ Automation Challange/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Automation_Challange.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Automation Challange/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Automation_Challange.Models
+{
+    public class User
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string eMail { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+
+        public string Address { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string JobTitle { get; set; }
+    }
+}
